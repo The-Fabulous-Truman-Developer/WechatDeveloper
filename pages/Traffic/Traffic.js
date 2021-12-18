3760a09572b0e965042447d3fb1a95f715faabb6
Serendipity <1789216340lhy@gmail.com>
2021-12-18 15:07:39 -0600

    Update the content

--- pages/Traffic/Traffic.js
+++ pages/Traffic/Traffic.js
@@ -62,5 +62,11 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+
+    EatPageChange:function(){
+        wx.navigateTo({
+            url: '../Traffic/Eat/eat'
+        })
     }
 })
