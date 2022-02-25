6ccb5d71373dd997ba73a6db57c64fbe0e6c4ffd
Serendipity <1789216340lhy@gmail.com>
2022-02-25 16:17:43 -0600

    modified:   source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.js 	modified:   source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.wxml 	new file:   source/pages/MainPage/InternationalContainer/TrumanIHouse/TrumanIHouse.js 	renamed:    source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.json -> source/pages/MainPage/InternationalContainer/TrumanIHouse/TrumanIHouse.json 	new file:   source/pages/MainPage/InternationalContainer/TrumanIHouse/TrumanIHouse.wxml 	renamed:    source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.wxss -> source/pages/MainPage/InternationalContainer/TrumanIHouse/TrumanIHouse.wxss 	modified:   source/pages/Traffic/Eat/eat.wxml Update the link

--- source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.js
+++ source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.js
@@ -1,84 +1,66 @@
-// pages/index/infoTrumanContainer/kirksvilleSurrounding/kirksvilleSurrounding.js
+// source/pages/MainPage/InternationalContainer/CISBulletinBoard/CISBulletinBoard.js
 Page({
 
-    /**
-     * Page initial data
-     */
-    data: {
+  /**
+   * 页面的初始数据
+   */
+  data: {
 
-    },
-  
-    /**
-     * Lifecycle function--Called when page load
-     */
-    onLoad: function (options) 
-    {
-      wx.downloadFile
-      ({
-        url: 
-        'https://636c-cloud1-0gbp4wfdcd43709a-1307090649.tcb.qcloud.la/static/Docs/Bulletin.docx?sign=6a9792ed97ee251aa3928eb641d2e7b4&t=1638305512',
+  },
 
-        success: function (res) 
-        {
-          const filePath = res.tempFilePath
-          wx.openDocument
-          ({
-            filePath: filePath,
-            success: function (res) 
-            {
-              console.log('打开文档成功')
-            }
-          })
-        }
-      })
-    },
-  
-    /**
-     * Lifecycle function--Called when page is initially rendered
-     */
-    onReady: function () {
-  
-    },
-  
-    /**
-     * Lifecycle function--Called when page show
-     */
-    onShow: function () {
-  
-    },
-  
-    /**
-     * Lifecycle function--Called when page hide
-     */
-    onHide: function () {
-  
-    },
-  
-    /**
-     * Lifecycle function--Called when page unload
-     */
-    onUnload: function () {
-  
-    },
-  
-    /**
-     * Page event handler function--Called when user drop down
-     */
-    onPullDownRefresh: function () {
-  
-    },
-  
-    /**
-     * Called when page reach bottom
-     */
-    onReachBottom: function () {
-  
-    },
-  
-    /**
-     * Called when user click on the top right corner to share
-     */
-    onShareAppMessage: function () {
-  
-    }
-  })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
