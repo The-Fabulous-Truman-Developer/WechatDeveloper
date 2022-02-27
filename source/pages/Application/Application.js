cffe36bab867c40484dd29ba45a30697a7efa710
jzh-Bill <783026426@qq.com>
2022-02-26 23:20:32 -0600

    tab switch

--- source/pages/Application/Application.js
+++ source/pages/Application/Application.js
@@ -16,4 +16,37 @@ Page({
         url: '../Application/DormRoomContainer/Dorm',
       })
     },
+    data: {
+      // tab 栏数据
+      tabs:[
+        {
+          id:0,
+          name:'已开始',
+          isActive:true,
+        },
+        {
+          id: 1,
+          name: '未开始',
+          isActive: false,
+        },
+        {
+          id: 2,
+          name: '待审批',
+          isActive: false,
+        },
+      ]
+    },
+  
+    // 切换 tab栏 选项
+    changeTab(e){
+      console.log(e)
+      let index = e.currentTarget.dataset.index;
+      let {tabs} = this.data;
+      tabs.forEach((item)=>{
+        item.id===index ? item.isActive=true : item.isActive=false;
+      });
+      this.setData({
+        tabs
+      });
+    },
 })
