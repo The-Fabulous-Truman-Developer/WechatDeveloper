0b066d82edd99447e1e8f7b8498f9c641abab381
jzh-Bill <37702019+jzh-Bill@users.noreply.github.com>
2022-02-28 16:21:52 -0600

    大改

--- source/pages/Application/Application.js
+++ source/pages/Application/Application.js
@@ -46,4 +46,14 @@ Page({
         tabs
       });
     },
+    EatPageChange: function () {
+      wx.navigateTo({
+        url: "./Eat/eat",
+      });
+    },
+    trafficInKirksville: function () {
+      wx.navigateTo({
+        url: "./Route/route",
+      });
+    },
 })
--- source/pages/Traffic/Traffic.js
+++ source/pages/Traffic/Traffic.js
@@ -1,13 +1,66 @@
-// pages/Application.js
+// source/pages/Traffic/Traffic.js
 Page({
-  EatPageChange: function () {
-    wx.navigateTo({
-      url: "./Eat/eat",
-    });
+
+  /**
+   * Page initial data
+   */
+  data: {
+
   },
-  trafficInKirksville: function () {
-    wx.navigateTo({
-      url: "./Route/route",
-    });
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
   },
-});
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
